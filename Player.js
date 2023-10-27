2bf008ebf49aba54449c8fb0b3f60c3a555dad96
tamasfabi <fabi.tamas.zoltan@gmail.com>
2023-10-27 17:21:26 +0200

    deploooooooy

--- Player.js
+++ Player.js
@@ -53,7 +53,7 @@ class Player {
     } else {
       const score = me.score();
       console.log({ score });
-      if (score > 8) {
+      if (score > 6) {
         bet(me.stack());
       }
       bet(30);
